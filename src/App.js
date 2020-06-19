4ffdb4f88751a38e4c7670459d76dfb8d8fb08ed
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-19 22:04:17 +0600

    Added Route guard to public, private and auth page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import {PrivateRoute, AuthPageRoute} from './route_guards/AppRouteGuards.route';
 import {APP_ROUTES} from './route_guards/App.routes';
+import SignInSide from "./components/SignIn/SignIn.component";
 
-const loginPage = () => (<h1>Login</h1>);
 const publicPage = () => (<h1>Public</h1>);
 const privatePage = () => (<h1>private</h1>);
 const pageNotFound = () => (<h1>404 Page not Found</h1>);
@@ -14,7 +14,7 @@ function App() {
         <div className="App">
             <Switch>
                 <Route exact path={APP_ROUTES.LANDING_PAGE} component={publicPage}/>
-                <AuthPageRoute exact path={APP_ROUTES.AUTH} component={loginPage}/>
+                <AuthPageRoute exact path={APP_ROUTES.AUTH} component={SignInSide}/>
                 <PrivateRoute exact path={APP_ROUTES.AUTHENTICATED_PAGE} component={privatePage}/>
                 <Route path='*' exact={true} component={pageNotFound}/>
             </Switch>
--- src/route_guards/AuthPageRoute.component.js
+++ src/route_guards/AuthPageRoute.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Redirect, Route} from "react-router-dom";
 import {APP_ROUTES} from './App.routes';
 
-const isAuthenticated = true;
+const isAuthenticated = false;
 const AuthPageRoute = ({component: Component, ...rest}) => (
     <Route {...rest} render={(props) => (
         isAuthenticated ? <Redirect to={APP_ROUTES.LANDING_PAGE}/> : <Component/>

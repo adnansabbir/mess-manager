90e21cc593adbae25f5c3f8f83f93eb465a381f6
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-23 04:50:31 +0600

    Auth based on current user

--- src/route_guards/AuthPageRoute.component.js
+++ src/route_guards/AuthPageRoute.component.js
@@ -1,12 +1,20 @@
 import React from "react";
 import {Redirect, Route} from "react-router-dom";
 import {APP_ROUTES} from './App.routes';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectCurrentUser} from "../redux/user/user.selector";
 
-const isAuthenticated = false;
-const AuthPageRoute = ({component: Component, ...rest}) => (
-    <Route {...rest} render={(props) => (
-        isAuthenticated ? <Redirect to={APP_ROUTES.LANDING_PAGE}/> : <Component {...props}/>
-    )}/>
-);
+const AuthPageRoute = ({component: Component, currentUser, ...rest}) => {
+    return (
+        <Route {...rest} render={(props) => (
+            currentUser ? <Redirect to={APP_ROUTES.LANDING_PAGE}/> : <Component {...props}/>
+        )}/>
+    )
+};
 
-export default AuthPageRoute;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+});
+
+export default connect(mapStateToProps)(AuthPageRoute);

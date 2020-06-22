1e0fe50c2d5a5d31be06868b27346f91e6abb86b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-23 04:50:23 +0600

    Auth based on current user

--- src/route_guards/PrivateRoute.component.js
+++ src/route_guards/PrivateRoute.component.js
@@ -1,12 +1,18 @@
 import React from "react";
 import {Redirect, Route} from "react-router-dom";
 import {APP_ROUTES} from './App.routes';
+import {createStructuredSelector} from "reselect";
+import {selectCurrentUser} from "../redux/user/user.selector";
+import {connect} from "react-redux";
 
-const isAuthenticated = false;
-const PrivateRoute = ({component: Component, ...rest}) => (
+const PrivateRoute = ({component: Component, currentUser, ...rest}) => (
     <Route {...rest} render={(props) => (
-        isAuthenticated ? <Component/> : <Redirect to={APP_ROUTES.AUTH}/>
+        currentUser ? <Component {...props}/> : <Redirect to={APP_ROUTES.AUTH}/>
     )}/>
 );
 
-export default PrivateRoute;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
